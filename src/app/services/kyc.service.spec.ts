4352be21166b15f0e3369be8b92c3e038fd8029c
Phoebe890 <123245247+Phoebe890@users.noreply.github.com>
2025-06-11 11:11:06 +0300

    test:add HttpClient testing

--- src/app/services/kyc.service.spec.ts
+++ src/app/services/kyc.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { KycService } from './kyc.service';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('KycService', () => {
   let service: KycService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+       providers: [provideHttpClientTesting()]
+    });
     service = TestBed.inject(KycService);
   });
 
